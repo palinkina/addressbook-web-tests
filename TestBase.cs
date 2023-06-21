3655ee395a7615dc7cc7577f814343c34ce39627
palin <palin@DESKTOP-CR7F8CD>
2023-06-21 23:35:42 +0300

    app

--- TestBase.cs
+++ TestBase.cs
@@ -3,18 +3,25 @@ using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium;
 using System;
 using System.Text;
-
+using System.Security.Policy;
 
 namespace WebAddressbookTests
 {
     public class TestBase
     {
+
+        protected Login_outHelper loginHelper;
+        protected NavigationHelper navigator;
+        protected GroupHelper groupHelp;
+        protected ContactHelper contacter;
+
         protected ApplicationManager app;
 
         [SetUp]
         public void SetupTest()
         {
             app = new ApplicationManager();
+
         }
 
         [TearDown]
@@ -28,12 +35,9 @@ namespace WebAddressbookTests
             {
                 // Ignore errors if unable to close the browser
             }
-            
+            Assert.AreEqual("", verificationErrors.ToString());
         }  
     }
 }
+     
 
-
-driver = new FirefoxDriver();
-baseURL = "http://localhost/addressbook/";
-verificationErrors = new StringBuilder();
