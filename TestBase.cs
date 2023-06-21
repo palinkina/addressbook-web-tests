00764fb9ee1bb490373ddcbdba72d55d977afe61
palin <palin@DESKTOP-CR7F8CD>
2023-06-21 23:26:05 +0300

    app

--- TestBase.cs
+++ TestBase.cs
@@ -9,24 +9,12 @@ namespace WebAddressbookTests
 {
     public class TestBase
     {
-        protected IWebDriver driver;
-        private StringBuilder verificationErrors;
-        protected string baseURL;
-        protected Login_outHelper loginHelper;
-        protected NavigationHelper navigator;
-        protected GroupHelper groupHelp;
-        protected ContactHelper contacter;
+        protected ApplicationManager app;
 
         [SetUp]
         public void SetupTest()
         {
-            driver = new FirefoxDriver();
-            baseURL = "http://localhost/addressbook/";
-            verificationErrors = new StringBuilder();
-            loginHelper = new Login_outHelper(driver);
-            navigator = new NavigationHelper(driver, baseURL);
-            groupHelp = new GroupHelper(driver);
-            contacter = new ContactHelper(driver);
+            app = new ApplicationManager();
         }
 
         [TearDown]
@@ -40,9 +28,12 @@ namespace WebAddressbookTests
             {
                 // Ignore errors if unable to close the browser
             }
-            Assert.AreEqual("", verificationErrors.ToString());
+            
         }  
     }
 }
-     
 
+
+driver = new FirefoxDriver();
+baseURL = "http://localhost/addressbook/";
+verificationErrors = new StringBuilder();
